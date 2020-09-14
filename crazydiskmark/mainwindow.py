80cc86627b921d10ad1144488d8c603dcf4f4cad
Fred Lins <fredcox@gmail.com>
2020-09-14 06:17:00 -0300

    ready to internacionalization. :rocket: :rabbit: :rocket: 🦖

--- crazydiskmark/mainwindow.py
+++ crazydiskmark/mainwindow.py
@@ -6,8 +6,11 @@ import humanfriendly
 from pathlib import Path
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
 from crazydiskmark.version import Version
+import gettext
 
 resource_path = os.path.dirname(__file__)
+t = gettext.translation('crazydiskmark', 'locale', fallback=True)
+_ = t.gettext
 
 
 class ThreadBenchmark(QtCore.QThread):
@@ -92,22 +95,22 @@ class ThreadBenchmark(QtCore.QThread):
 
     def threadFinished(self):
         pass
-        print('Thread Finished, garbage collecting now...')
+        print(_('Thread Finished, garbage collecting now...'))
         targetFilename = f'{self.target}/{self.tmpFile}'
-        print(f'Verifying if temp file exists {targetFilename}')
+        print('{} {}'.format(_('Verifying if temp file exists'), targetFilename))
         if os.path.isfile(targetFilename):
-            print(f'Yes, removing the file: [{targetFilename}]')
+            print('{} [{}]'.format(_('Yes, removing the file:'), targetFilename))
             os.remove(targetFilename)
 
     def run(self):
         # executing Benchmarks
-        print('Executing Benchmarks....')
+        print(_('Executing Benchmarks....'))
         for index, operation in enumerate(self.operations):
-            print(f'Running index: [{index}]\tprefix:\t[{operation["prefix"]}] rw:[{operation["rw"]}]')
+            print('{} {}\t [{}] [{}]'.format(_('Running index:'), index, operation["prefix"], operation["rw"]))
             filename = f'--filename="{self.target}/{self.tmpFile}"'
             name = f'--name={operation["prefix"]}{operation["rw"]}'
             currentCmd = f'{self.fioWhich} {self.loops} {self.size} {filename} {self.stoneWall} {self.ioEngine} {self.direct} {self.zeroBuffers} {name} --bs={operation["bs"]} --iodepth={operation["ioDepth"]} --numjobs={operation["numJobs"]} --rw={operation["rw"]} {self.outputFormat}'
-            print(f'Executing Command: {currentCmd}')
+            print('{} {}'.format(_('Executing Command:'), currentCmd))
             output = json.loads(subprocess.getoutput(currentCmd).encode('utf-8'))
             if 'read' in operation['rw']:
                 self.bw_bytes = '{}/s'.format(humanfriendly.format_size(output['jobs'][0]['read']['bw_bytes']))
@@ -146,6 +149,38 @@ class MainWindow(QtWidgets.QMainWindow):
         self.actionAbout.triggered.connect(self.showAboutDialog)
         self.progressBar = self.findChild(QtWidgets.QProgressBar, 'progressBar')
 
+        # ReadLabel
+        labelStyleSheet = """
+        font: 57 "Ubuntu";
+        font-weight: bold;
+        font-size: 15pt;
+        """
+        readLabel = self.findChild(QtWidgets.QLabel, 'ReadLabel')
+        readLabel.setText(_('Read'))
+        readLabel.setStyleSheet(labelStyleSheet)
+        readLabel.setAlignment(QtCore.Qt.AlignHCenter)
+        # writeLabel
+        writeLabel = self.findChild(QtWidgets.QLabel, 'writeLabel')
+        writeLabel.setStyleSheet(labelStyleSheet)
+        writeLabel.setAlignment(QtCore.Qt.AlignHCenter)
+        writeLabel.setText(_('Write'))
+        # directoryLabel
+        directoryLabel = self.findChild(QtWidgets.QLabel, 'directoryLabel')
+        directoryLabel.setText(_('Directory select'))
+        self.selectPushButton.setText(_('Select'))
+
+        menuFile = self.findChild(QtWidgets.QMenu, 'menuFile')
+        menuFile.setTitle(_('File'))
+
+        actionQuit = self.findChild(QtWidgets.QAction, 'actionQuit')
+        actionQuit.setText(_('Quit'))
+
+        menuHelp = self.findChild(QtWidgets.QMenu, 'menuHelp')
+        menuHelp.setTitle(_('Help'))
+
+        actionAbout = self.findChild(QtWidgets.QAction, 'actionAbout')
+        actionAbout.setText(_('About'))
+
         self.labelWidgets.append(self.findChild(QtWidgets.QLabel, 'seq1mq8t1ReadLabel'))
         self.labelWidgets.append(self.findChild(QtWidgets.QLabel, 'seq1mq8t1WriteLabel'))
         self.labelWidgets.append(self.findChild(QtWidgets.QLabel, 'seq1mq1t1ReadLabel'))
@@ -171,6 +206,15 @@ class MainWindow(QtWidgets.QMainWindow):
         uic.loadUi(f'{resource_path}/aboutdialog.ui', self.aboutDialog)
         self.okPushButton = self.aboutDialog.findChild(QtWidgets.QPushButton, 'okPushButton')
         self.okPushButton.clicked.connect(self.quitAboutDialog)
+        # authorLabel
+        authorLabel = self.aboutDialog.findChild(QtWidgets.QLabel, 'authorLabel')
+        authorLabel.setText(_('Author: Fred Lins <fredcox@gmail.com>'))
+        # warrantyLabel
+        warrantyLabel = self.aboutDialog.findChild(QtWidgets.QLabel, 'warrantyLabel')
+        warrantyLabel.setText(_('This program comes with absolutely no warranty.'))
+        # licenseLabel
+        licenseLabel = self.aboutDialog.findChild(QtWidgets.QLabel, 'licenseLabel')
+        licenseLabel.setText(_('License'))
 
         labelVersion = self.aboutDialog.findChild(QtWidgets.QLabel, 'versionLabel')
         labelVersion.setText(self.version.getVersion())
@@ -182,36 +226,36 @@ class MainWindow(QtWidgets.QMainWindow):
         self.show()
 
     def receiveThreadBenchmark(self, val):
-        print(f'Receiving ===> {val}')
+        print('{} {}'.format(_('Receiving ===>'), val))
         self.labelWidgets[self.operationIndex].setText(val)
         self.progressBar.setProperty('value', (self.operationIndex + 1) * 12.5)
         self.operationIndex += 1
         if self.operationIndex == len(self.thread.operations):
-            self.startPushButton.setText('Start')
-            self.statusbar.showMessage('IDLE')
+            self.startPushButton.setText(_('Start'))
+            self.statusbar.showMessage(_('IDLE'))
             self.operationIndex = 0
 
     def startBenchMark(self):
         if self.thread.isRunning():
-            self.startPushButton.setText('Start')
-            self.statusbar.showMessage('IDLE')
-            print('Stopping Thread....')
+            self.startPushButton.setText(_('Start'))
+            self.statusbar.showMessage(_('IDLE'))
+            print(_('Stopping Thread....'))
             self.thread.terminate()
             self.operationIndex = 0
         else:
             self.clearResults()
             # Verify if directory is writable
             if self.isWritable():
-                self.startPushButton.setText('Stop')
-                print('Starting benchmark...')
-                self.statusbar.showMessage('Running. Please wait, this may take several minutes...')
-                print('Directory writable. OK [Starting Thread]')
+                self.startPushButton.setText(_('Stop'))
+                print(_('Starting benchmark...'))
+                self.statusbar.showMessage(_('Running. Please wait, this may take several minutes...'))
+                print(_('Directory writable. OK [Starting Thread]'))
                 self.thread.start()
             else:
-                print('Directory not writable. [ERROR]')
+                print(_('Directory not writable. [ERROR]'))
 
     def clearResults(self):
-        self.statusbar.showMessage('IDLE')
+        self.statusbar.showMessage(_('IDLE'))
         self.thread.operationsIndex = 0
         self.progressBar.setProperty('value', 0)
         for label in self.labelWidgets:
@@ -219,25 +263,24 @@ class MainWindow(QtWidgets.QMainWindow):
 
     # show directory dialog
     def showDirectoryDialog(self):
-        print('Show Directory Dialog!')
         dialog = QtWidgets.QFileDialog()
         dialog.setFileMode(QtWidgets.QFileDialog.DirectoryOnly)
         if dialog.exec():
             self.thread.target = dialog.selectedFiles()[0]
-            print(f'Directory ===> {self.thread.target}')
+            print('{} {}'.format(_('Directory ===>'), self.thread.target))
             self.directoryLineEdit.setText(self.thread.target)
 
     def isWritable(self):
         self.thread.target = self.directoryLineEdit.text()
-        print(f'Verify if dir {self.thread.target} is writable...')
+        print('{} {}...'.format(_('Verify dir'), self.thread.target))
         if os.access(self.thread.target, os.W_OK):
-            print(f'{self.thread.target} is writable.')
+            print('{} {}'.format(self.thread.target, _('is writable.')))
             return True
         else:
-            print(f'{self.thread.target} NOT writable.')
+            print('{} {}'.format(self.thread.target, _('NOT writable.')))
             errorDialog = QtWidgets.QMessageBox()
             errorDialog.setIcon(QtWidgets.QMessageBox.Warning)
-            errorDialog.setText(f'Cannot write to directory {self.thread.target}')
+            errorDialog.setText('{} {}'.format(_('Cannot write to directory'), self.thread.target))
             errorDialog.exec()
             return False
 
